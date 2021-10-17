43587fabc01a6685ef74c0ee3fcee53e436c55e3
31Vector31 <Y..u..r..a..2002>
2021-10-17 22:22:34 +0300

    added push/pop to CustomArray

--- CustomArray.js
+++ CustomArray.js
@@ -2,4 +2,21 @@ class CustomArray {
     constructor(...args) {
         this.array = args;
     }
+
+    push(...args) {
+        for (let i = this.array.length, j = 0; j < args.length; i++, j++) {
+            this.array[i] = args[j];
+        }
+        return this.array.length;
+    }
+
+    pop() {
+        let lastElement = this.array[this.array.length - 1];
+        let temp = [];
+        for (let i = 0; i < this.array.length - 1; i++) {
+            temp[i] = this.array[i];
+        }
+        this.array = temp;
+        return lastElement;
+    }
 }
