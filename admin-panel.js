7176a2c748e4287b63dba368801f6438de4fe91d
31Vector31 <Y..u..r..a..2002>
2021-10-21 15:32:16 +0300

    fixed admin panel

--- admin-panel.js
+++ admin-panel.js
@@ -1,22 +1,19 @@
+let localStorageKey = "users";
 let indexEditElement;
 dataOutput();
 
 let save = document.getElementById('admin-panel__save');
 save.onclick = () => {
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
+    let users = localStorageGetItem(localStorageKey);
     let username = document.getElementById('admin-panel__username').value.trim();
     if (username == "") return;
     let department = document.getElementById('admin-panel__department').value;
     let currentDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
     if (indexEditElement !== undefined) {
-        users.forEach(function (user, i) {
-            if (i == indexEditElement) {
-                user.username = username;
-                user.department = department;
-                user.updateDate = currentDate;
-                indexEditElement = undefined;
-            }
-        });
+        users[indexEditElement].username = username;
+        users[indexEditElement].department = department;
+        users[indexEditElement].updateDate = currentDate;
+        indexEditElement = undefined;
     } else {
         let user = {
             username: username,
@@ -26,14 +23,22 @@ save.onclick = () => {
         };
         users.push(user);
     }
-    localStorage.setItem("users", JSON.stringify(users));
+    localStorageSetItem(localStorageKey, users);
     dataOutput();
     document.getElementById('admin-panel__username').value = "";
     document.getElementById('admin-panel__department').value = document.getElementById('admin-panel__department').options[0].value;
-};
+}
+
+let table = document.getElementById('admin-panel__table');
+table.onclick = (event) => {
+    let target = event.target;
+    let index = target.dataset.index;
+    if (target.className === 'admin-panel__editButton') editUser(index);
+    if (target.className === 'admin-panel__deleteButton') deleteUser(index);
+}
 
 function dataOutput() {
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
+    let users = localStorageGetItem(localStorageKey);
     let table = "";
     users.forEach((user, index) => {
         table += `<tr>
@@ -41,30 +46,44 @@ function dataOutput() {
         <td>${user.department}</td>
         <td>${user.creationDate}</td>
         <td>${user.updateDate}</td>
-        <td><button onclick="editUser('${index}')">Редактировать</button></td>
-        <td><button onclick="deleteUser('${index}')">Удалить</button></td>
+        <td><button class="admin-panel__editButton" data-index="${index}">Редактировать</button></td>
+        <td><button class="admin-panel__deleteButton" data-index="${index}">Удалить</button></td>
     </tr>`;
     });
     document.getElementById('admin-panel__table').innerHTML = table;
 }
 
 function deleteUser(index) {
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
+    let users = localStorageGetItem(localStorageKey);
     if (!confirm(`Вы уверенны, что хотите удалить пользователя ${users[index].username}?`)) {
         return;
     }
     users.splice(index, 1);
-    localStorage.setItem("users", JSON.stringify(users));
+    localStorageSetItem(localStorageKey, users);
     dataOutput();
 }
 
 function editUser(index) {
-    let users = JSON.parse(localStorage.getItem("users") || "[]");
-    users.forEach(function (user, i) {
-        if (i == index) {
-            document.getElementById('admin-panel__username').value = user.username;
-            document.getElementById('admin-panel__department').value = user.department;
-            indexEditElement = index;
-        }
-    });
+    let users = localStorageGetItem(localStorageKey);
+    document.getElementById('admin-panel__username').value = users[index].username;
+    document.getElementById('admin-panel__department').value = users[index].department;
+    indexEditElement = index;
+}
+
+function localStorageSetItem(key, value) {
+    try {
+        localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+        alert(e);
+    }
+}
+
+function localStorageGetItem(key) {
+    try {
+        let value = JSON.parse(localStorage.getItem(key) || "[]");
+        return Array.isArray(value) ? value : [];
+    } catch (e) {
+        alert(e);
+        return [];
+    }
 }
